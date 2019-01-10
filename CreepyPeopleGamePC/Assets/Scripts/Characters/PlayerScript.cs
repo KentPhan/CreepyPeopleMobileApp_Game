5659716c48af17b4b14f794eb4bdfbbe74e01ca5
KentPhan <u0734504@ad.utah.edu>
2019-01-10 15:58:12 -0700

    Moved some stuff

--- CreepyPeopleGamePC/Assets/Scripts/Characters/PlayerScript.cs
+++ CreepyPeopleGamePC/Assets/Scripts/Characters/PlayerScript.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Assets.Scripts.Characters
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Player : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
