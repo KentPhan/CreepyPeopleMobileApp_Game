ff38aef7ec8ce2bde9879f6ff64d4d572e708e9e
KentPhan <kent.chi.lam@gmail.com>
2019-01-11 13:08:40 -0700

    Set up basic server logic for testing

---
+++ CreepyPeopleGameMain/Assets/NetworkManagerScript.cs
@@ -0,0 +1,64 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace Assets
+{
+    public class NetworkManagerScript : MonoBehaviourPunCallbacks
+    {
+        public string versionName = "0.1";
+
+        public GameObject startScreen, connectedScreen, disconnectedScreen;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void ConnectToNetwork()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+
+
+            Debug.Log("Connecting to Network...");
+        }
+
+
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinLobby(TypedLobby.Default);
+
+            Debug.Log("Connected to Master");
+        }
+
+
+
+        public override void OnJoinedLobby()
+        {
+            startScreen.SetActive(false);
+            connectedScreen.SetActive(true);
+
+            Debug.Log("Joined Lobby");
+        }
+
+        private void OnFailedToConnectToPhoton()
+        {
+            if (startScreen.activeSelf)
+                startScreen.SetActive(false);
+
+            if (connectedScreen.activeSelf)
+                connectedScreen.SetActive(false);
+
+            disconnectedScreen.SetActive(true);
+
+            Debug.Log("Disconnnected from Network...");
+        }
+    }
+}
